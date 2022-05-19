48ddb8ccb9e134ba040cf6b0967e7e9056895154
kd071090 <kierandickerson3@gmail.com>
2022-05-19 22:14:25 +0100

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const http = require('http');
 
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        //...
+    }
+}
+
+if (req.url === '/api/course') {
+    //...
+}
+});
+
+server.listen(3000);
